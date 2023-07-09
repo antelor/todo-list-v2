b8acf082be0a6129a294d7ac0d9977ba88cbf3d8
joaquinarruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-07-09 14:39:41 -0300

    added task render

--- src/item.js
+++ src/item.js
@@ -21,4 +21,24 @@ export class Item{
     setPriority(newPriority){
         this.priority = newPriority;
     }
+
+    renderItem(){
+        let itemCard = document.createElement('div');
+        itemCard.classList.add('itemCard');
+
+        let itemName = document.createElement('h1');
+        itemName.textContent = this.name;
+        itemCard.appendChild(itemName);
+
+        let itemDesc = document.createElement('p');
+        itemDesc.textContent = this.desc;
+        itemCard.appendChild(itemDesc);
+
+        let itemDueDate = document.createElement('p2');
+        itemDueDate.textContent = this.dueDate;
+        itemCard.appendChild(itemDueDate);
+
+        return itemCard;
+
+    }
 }
